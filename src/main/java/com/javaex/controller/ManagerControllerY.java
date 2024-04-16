be1ace9e0603c1d5365e0b958a568ccb67a0930b
aassdd236 <aassdd236@naver.com>
2024-04-16 17:49:50 +0900

    d

--- src/main/java/com/javaex/controller/ManagerControllerY.java
+++ src/main/java/com/javaex/controller/ManagerControllerY.java
@@ -1,30 +1,6 @@
 package com.javaex.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-
-import com.javaex.service.ManagerServiceY;
-import com.javaex.util.JsonResult;
-import com.javaex.vo.ManagerVoY;
-
-@RestController
 public class ManagerControllerY {
 
-	@Autowired
-	private ManagerServiceY managerServiceY;
-
-	@PostMapping("/home/manager/insert")
-	public JsonResult insert(@RequestParam("files") MultipartFile[] files,
-			@RequestParam("profile") MultipartFile profileFile, @ModelAttribute ManagerVoY managerVoY) {
-		System.out.println("ManagerControllerY.insert()");
-
-		managerServiceY.exeInsert(managerVoY);
-
-		return JsonResult.success("Data inserted successfully.");
-	}
 
 }
