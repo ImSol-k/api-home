5b4c285d74626b43ddc86c599468e68219d696a5
ojiwon99 <dhwldnjs0420@naver.com>
2024-04-16 17:47:15 +0900

    지원이 수정

--- src/main/java/com/javaex/service/Jw_LoginService.java
+++ src/main/java/com/javaex/service/Jw_LoginService.java
@@ -1,19 +1,8 @@
-// Jw_LoginService.java
+
 package com.javaex.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-import com.javaex.dao.Jw_LoginDao;
-import com.javaex.vo.Jw_Vo;
-
-@Service
 public class Jw_LoginService {
 
-    @Autowired
-    private Jw_LoginDao jw_LoginDao;
 
-    public Jw_Vo exelogin(String id, String password) {
-        return jw_LoginDao.Jw_Loginlogin(id, password);
-    }
 }
